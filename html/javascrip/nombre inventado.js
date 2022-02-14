dfd21df4b1a09b9b520366776e66bb6379642b6e
CarmenAlida <cfermin@uoc.edu>
2022-02-14 23:24:15 +0100

    sintasis

--- html/javascrip/nombre inventado.js
+++ html/javascrip/nombre inventado.js
@@ -1,1 +1,27 @@
-alert("hola mundo")
+/*//alert("BIENVENIDOS")
+//declarar
+/let a;
+let b;
+//asignar
+a=10;
+b=20;
+
+//calculo
+let total= a + b;
+//mostrar por pantalla
+console.log("Total " + total);*/
+
+
+
+
+
+//declaració de la variable
+var a;
+//assignació del valor 17
+a=17;
+//declaració i assignació de la variable
+var b=17;
+//també es pot assignar un valor des de la pantalla
+var c=prompt("Introdueix un valor");
+console.log(c);
+ alert(c);
