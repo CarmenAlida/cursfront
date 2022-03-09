fffa45b91c5ff660a1b6769918ea602a32e37373
CarmenAlida <cfermin@uoc.edu>
2022-03-09 19:48:33 +0100

    while

---
+++ proyecto/js/index.js
@@ -0,0 +1,59 @@
+function buy(id) {
+    console.table(products[id-1]);
+    
+    cart.push(products[id-1]);
+
+    console.log(cart);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // total de la compra
+// var total = 0;
+
+// //Exercici 0: Mostrar cartList
+
+// function ShowCartList() {
+//     console.table(products);
+//     console.table(cartList);
+// }
+
+// function showPrinces() {
+//     console.table(subtotal);
+// }
+//  function showCart() {
+//      console.table(cart);
+//  }
+
+//  // Exercise 1
+//  function buy(id){
+//      cartList.push(products[id . 1]);
+//  }
