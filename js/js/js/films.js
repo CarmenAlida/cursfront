b23a9433b39daf41403531298e236cd326461882
CarmenAlida <cfermin@uoc.edu>
2022-03-16 23:46:04 +0100

    pelis

--- js/js/js/films.js
+++ js/js/js/films.js
@@ -1,4 +1,6 @@
-function getAllDirectors(array) {
+
+    // 1
+    function getAllDirectors(array) {
     let result = [];
     result = array.map(movie =>{
         return movie.director;
@@ -6,20 +8,46 @@ function getAllDirectors(array) {
     console.table(result);
     return result;
 }
-
+// 2
 function getMoviesFromDirector(array,director) {
     const films = array.filter(movie => movie.director == director);
     console.table(films);
     return films;
 }
-
-function moviesAverageOfDirector(director) {
-    const filmsBydirector = movies.filter(movie => movie.director == director);
+// 3
+function moviesAverageOfDirector(array, director) {
+    const films = array.filter(movie => movie.director == director);
     console.table("Director: " + director);
-    console.table(filmsBydirector);
+    console.table(films);
 
-    const scores = filmsBydirector.reduce((contador, film) => {
-        console.log("film: " + filmsBydirector.title + " Score: "+ filmsBydirector.score);
-        return contador + filmsBydirector.score;
+    const scores = films.reduce((contador, film) => {
+        console.log("film: " + film.title + " Score: "+ film.score);
+        return contador + film.score;
     }, 0);
 }
+// media dejando 2 decimales
+const media = parseFloat(scores / parseInt(films.length)).toFixed(2);
+console.log("Total Scores " + parseFloat(scores.toFixed(2)))+"numero de peliculas" + parseInt(films.length);
+console.log('media sores: '+ media);
+ 
+// 4
+function orderAlphabetically(array){ 
+array.sort(function (a, b) {
+    if (a.title >b.title) {
+        return 1;
+    }
+    if (a.title <b.title) {
+        return 1;
+    }
+    return 0;
+});
+
+const primeros = movies.slice(0,20);
+console.table(primeros);
+return primeros;
+}
+// 5
+function orderByYear(array) {
+    let result = []; 
+    result = array.map(movie => ({year: movie.year, title: movie.title}))
+};
