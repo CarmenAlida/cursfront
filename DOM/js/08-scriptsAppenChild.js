da4a545036b23b0cecdefa521256fad4642ed23d
CarmenAlida <cfermin@uoc.edu>
2022-03-18 12:00:47 +0100

    film 8 y 9

--- DOM/js/08-scriptsAppenChild.js
+++ DOM/js/08-scriptsAppenChild.js
@@ -1,3 +1,13 @@
 // Exercici 8: Crear un nou link en el menú de Navegació principal
 // Amb appendChild crearem una nova opció de navegació en el menú principal
 
+const enlace = document.createElement('A');
+
+enlace.textContent = 'nuevo enlace';
+
+enlace.href = '/nuevo enlace';
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(enlace);
+
+console.log(enlace);
--- DOM/js/09-scripts_NewCard.js
+++ DOM/js/09-scripts_NewCard.js
@@ -1,3 +1,4 @@
+// Exercici 9: Crear una Nova card
 // Amb appendChild crearem una nova card, amb el següent contingut html:
 // <div class="card">
 //     <img src="img/hacer2.jpg">
@@ -7,3 +8,41 @@
 //             <p class="precio">$800 por persona</p>
 //         </div>
 //  </div>
+
+const p1 = document.createElement('P');
+p1.textContent = 'Karmen';
+p1.classList.add('categoria');
+p1.classList.add('concierto');
+
+const p2 = document.createElement('P');
+p2.textContent = 'Visita a Karmen';
+p2.classList.add('titulo');
+
+const p3 = document.createElement('p');
+p3.textContent = '300Bs';
+p3.classList.add('precio');
+
+const info = document.createElement('div');
+info.classList.add('info');
+info.appendChild(p1);
+info.appendChild(p2);
+info.appendChild(p3);
+
+const imagen = document.createElement('img');
+imagen.src = 'img/hacer3.jpg';
+
+const card = document.createElement('div');
+card.classList.add('card');
+
+card.appendChild(imagen);
+
+card.appendChild(info);
+
+const contenedor = document.querySelector('.hacer .contenedor-cards');
+contenedor.appendChild(card);
+
+
+
+
+console.log(p1);
+console.log(p2);
