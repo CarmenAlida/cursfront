5036addedb8fb876dfa35ba974a1c350e770bc65
CarmenAlida <cfermin@uoc.edu>
2022-03-31 23:25:58 +0200

    1erVUE

---
+++ VUE/clase/app.js
@@ -0,0 +1,26 @@
+// const app = new Vue({
+//     el: '#app'
+//     template:'#ejemplo',
+//     data:{
+//         mensaje:"<b> mensaje</b>",
+//         mensajeHTML: "<b> mensaje</b>",
+//         imagen: "img/medanos.jpg",
+//     }
+//    });
+   
+const app = new Vue({
+    el: '#app',
+    template: '#mensaje',
+    data:{
+        mensaje: 'hola mundo',
+        mensajeHTML: 'Mi primer vue.js',
+        imagen: "img/medanos.jpg",
+        url: "https://www.uoc.edu/portal/es/index.html/",
+    },
+    methods: {
+        // creando una función
+        
+        
+    },
+   });
+    
