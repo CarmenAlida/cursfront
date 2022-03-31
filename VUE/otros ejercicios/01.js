464b2c7411e4880f4a1de3c187404275c8ef66d5
CarmenAlida <cfermin@uoc.edu>
2022-03-31 23:27:30 +0200

    1erVUE

---
+++ VUE/otros ejercicios/01.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        titulo: 'hola mundo con Vue',
+        frutas:[
+            {nombre: 'pera', cantidad:10},
+            {nombre: 'lulo', cantidad:0},
+            {nombre: 'yogurt', cantidad:11}
+        ]
+    }
+})
