826e8fd2c2b149083c29f7e60b8296836c0563d3
CarmenAlida <cfermin@uoc.edu>
2022-04-01 18:10:50 +0200

    vue

--- VUE/otros ejercicios/01.js
+++ VUE/otros ejercicios/01.js
@@ -2,10 +2,29 @@ const app = new Vue ({
     el: '#app',
     data: {
         titulo: 'hola mundo con Vue',
-        frutas:[
+        frutas: [
             {nombre: 'pera', cantidad:10},
             {nombre: 'lulo', cantidad:0},
             {nombre: 'yogurt', cantidad:11}
-        ]
+        ],
+        nuevaFruta: '',
+        total: 0
+    },
+    methods:{
+        agregarFruta () {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            });
+            this.nuevaFruta = '';
+        }
+    },
+    computed: {
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
     }
 })
