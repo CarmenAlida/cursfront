fd32a4e361ae6765c774631f5c13de77896e4eea
CarmenAlida <cfermin@uoc.edu>
2022-04-04 18:43:55 +0200

    CambioMoneda

---
+++ VUE/otros ejercicios/ciclo-vida.js
@@ -0,0 +1,35 @@
+new Vue({
+    el:'#app',
+    data: {
+       saludo: 'ciclo de vida con vue'
+    },
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        console.log('created');
+    },
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate');
+    },
+    updated() {
+        console.log('updated');
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy');
+    },
+    destroyed() {
+        console.log('destroyed');
+    },
+    methods: {
+        destruir(){
+            this.$destroy();
+        }
+    },
+})
